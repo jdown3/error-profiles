9a9c45ba1a434801f571ad4dc5d951ff6b0be4a9
jdown3 <julia.down@student.uts.edu.au>
2016-12-20 17:14:42 +1100

    flank count

--- errors.py
+++ errors.py
@@ -1,6 +1,6 @@
 import sys
-sys.stdout=open('Count of error types table.txt', 'w')#w means write, output will print to file instead of console, global 
 
+filename=r'data\Jurkat_only_S5.consensus.100.fastq.gz' #what do about r, people won't input
 #LETTERS CODE to consider multiple letter changes in one error/diff
 letters=['A','C','T','G'] #creates a list
 counter={'AC':0, 'CA':0,'AG':0, 'GA':0,'AT':0, 'TA':0,'CT':0, 'TC':0, 'CG':0, 'GC':0,'GT':0, 'TG':0} #creates a dictionary? of counters
@@ -9,16 +9,29 @@ ctable[0].append('Dif')
 ctable[1].append('Con')
 ctable[2].append('No.')
 
-ftable=[[],[],[],[],[]] 
+flankno=2
+ftable=[[],[],[],[],[],[]] 
 ftable[0].append('Seq')
-ftable[1].append('Er')
-ftable[2].append('Typ')
-ftable[3].append('Pre')
-ftable[4].append('Post')
+ftable[1].append('Err')
+ftable[2].append('Pre')
+ftable[3].append('Dif')
+ftable[4].append('Con')
+ftable[5].append('Post')
+from collections import defaultdict
+fcounter=defaultdict(int)
+#initialise counter
+#fcounter={prev+l+cons+post:0,prev+l+cons:0,l+cons+post:0,prev+post:0,prev:0,post:0}
+
+cftable=[[],[],[],[],[]]
+cftable[0].append('Pre')
+cftable[1].append('Dif')
+cftable[2].append('Con')
+cftable[3].append('Pst')
+cftable[4].append('No.')
 
 import re #using reg exp to find broken seq
 import gzip
-with gzip.open(r'data\Jurkat_only_S5.consensus.100.fastq.gz', 'rt') as f:	
+with gzip.open(filename, 'rt') as f:	
 	for i, line in enumerate(f, 1): #contents is file as a string
 		if i % 4 == 2:
 			seq=line #store the seq
@@ -26,7 +39,7 @@ with gzip.open(r'data\Jurkat_only_S5.consensus.100.fastq.gz', 'rt') as f:
 			errorno={seqno:0} 
 			nextline= True #nextline, how specify, add 1 to i?
 			if nextline: #inside the loop
-				diffsline = gzip.open(r'data\Jurkat_only_S5.consensus.100.fastq.gz', 'rt').readlines()[i] #reads file starting at 0
+				diffsline = gzip.open(filename, 'rt').readlines()[i] #reads file starting at 0
 				diffs=diffsline.split() #makes a list from changes line, separated by spaces 
 			nextline= False
 			for d in diffs: #and s in seqs to get them to align?
@@ -42,25 +55,40 @@ with gzip.open(r'data\Jurkat_only_S5.consensus.100.fastq.gz', 'rt') as f:
 								errorno[seqno]+=1 #counts diffs for each diffsline #DIFFFFFFFFF
 								ftable[1].append(str(errorno[seqno]))
 								ftable[0].append(str(seqno))
-								ftable[2].append(l+cons) #all lists of string
-								#go back and get chars, shorter way??
+								ftable[3].append(l) #all lists of string
+								ftable[4].append(cons)
+								#go back and get chars, shorter way? slices
 								if posno==0: # == checks if two things have the same value
-									prev2=' '
-									post2=seq[posno+1]+seq[posno+2]
-								elif posno==1:
-									prev2=' '+seq[0]
-									post2=seq[posno+1]+seq[posno+2]
-								elif len(seq)-1<posno+1: #-1 to manage offset, make same as posno, start at 0
-									post2=' '
-									prev2=seq[posno-2]+seq[posno-1]	
-								elif len(seq)-1<posno+2: 
-									post2=seq[posno+1]+' '
-									prev2=seq[posno-2]+seq[posno-1]
+									prev=' '
+									post=seq[posno+1:(posno+1+flankno)] #slicing is inclusive of bounds, if flankno 2, like saying 1:2
+								elif posno+1<=flankno: 
+									prev=' '+seq[:posno]
+									post=seq[posno+1:(posno+1+flankno)]
+								elif len(seq)-1==posno: #-1 to manage offset, make same as posno, start at 0
+									post=' ' #going to a newline???
+									prev=seq[posno-2:posno]	
+								elif posno+1>(len(seq)-flankno): #+1 to make it same as flankno, starting at 1
+									post=seq[posno+1:]+' '
+									prev=seq[posno-2:posno]
 								else:
-									prev2=seq[posno-2]+seq[posno-1]
-									post2=seq[posno+1]+seq[posno+2]
-								ftable[3].append(prev2)
-								ftable[4].append(post2)
+									prev=seq[posno-2:posno]
+									post=seq[posno+1:(posno+1+flankno)]
+								ftable[2].append(prev)
+								ftable[5].append(post)
+								#sys.stdout=open('flank table.txt', 'w')
+								#for row in zip(ftable[0],ftable[1],ftable[2],ftable[3],ftable[4],ftable[5]): #wants a str input #3rd element requires iteration? as many rows to match up with the first two arguments
+									#print('\t'.join(row)) #doesn't cumulative print
+								#increment counter for that error seq
+								fcounter[prev+l+cons+post]+=1
+								fcounter[prev+l+cons+'..']+=1
+								fcounter['..'+l+cons+post]+=1
+								fcounter[prev+'.'+'.'+post]+=1
+								fcounter[prev+'.'+'.'+'..']+=1
+								fcounter['..'+'.'+'.'+post]+=1
+
+								print (fcounter)
+
+	sys.stdout=open('Count of error types table.txt', 'w')#w means write, output will print to file instead of console, global 
 	for c in counter: #each counter is a line in the table
 		#list of lists method
 		splitc = re.findall('.', c) #splits c after a single position
@@ -72,15 +100,18 @@ with gzip.open(r'data\Jurkat_only_S5.consensus.100.fastq.gz', 'rt') as f:
 	for row in zip(ctable[0],ctable[1],ctable[2]): #wants a str input #3rd element requires iteration? as many rows to match up with the first two arguments
 		print('\t'.join(row)) #tab separated 
 
-sys.stdout=open('flank table.txt', 'w')
-for row in zip(ftable[0],ftable[1],ftable[2],ftable[3],ftable[4]): #wants a str input #3rd element requires iteration? as many rows to match up with the first two arguments
-	print('\t'.join(row))
-
-	#dataframe method to get headers, need to install pandas
-	#import pandas
-	#df=pandas.DataFrame(table[0],table[1],table[2])
-	#df.columns["Diff","Cons","Count"]
-	#print (df) 
+	print('\n')
+	#count flanks table, ONLY GOT 1 RESULT 
+	for k in fcounter: #allow for differing key size
+		splitk = re.findall('.',k) #len is 6 elements, even when you get full stops
+		cftable[0].append(splitk[0]+splitk[1]) 
+		cftable[1].append(splitk[2])
+		cftable[2].append(splitk[3])
+		cftable[3].append(splitk[4]+splitk[5]) #out of range error??
+		cftable[4].append(str(fcounter[k]))
+	for row in zip(cftable[0],cftable[1],cftable[2],cftable[3],cftable[4]):
+		print('\t'.join(row)) 
 
 #NEXT
+#fix count flanks, not working
 #run on whole file
