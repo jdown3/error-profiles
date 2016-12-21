54cee8080b0684eae739b1ff2f11932ad35cedce
jdown3 <julia.down@student.uts.edu.au>
2016-12-21 12:56:28 +1100

    flank table works, order

--- errors.py
+++ errors.py
@@ -9,19 +9,19 @@ ctable[0].append('Dif')
 ctable[1].append('Con')
 ctable[2].append('No.')
 
-flankno=2
-ftable=[[],[],[],[],[],[]] 
-ftable[0].append('Seq')
-ftable[1].append('Err')
-ftable[2].append('Pre')
-ftable[3].append('Dif')
-ftable[4].append('Con')
-ftable[5].append('Post')
-from collections import defaultdict
-fcounter=defaultdict(int)
-#initialise counter
-#fcounter={prev+l+cons+post:0,prev+l+cons:0,l+cons+post:0,prev+post:0,prev:0,post:0}
+flankno=2  
+ftable=[[],[],[],[],[],[],[]] 
+ftable[0].append('Se#') 
+ftable[1].append('Di#')
+ftable[2].append('Er#')
+ftable[3].append('Pre')
+ftable[4].append('Dif')
+ftable[5].append('Con')
+ftable[6].append('Post')
 
+from collections import defaultdict
+from collections import OrderedDict
+cfcounter=defaultdict(int)
 cftable=[[],[],[],[],[]]
 cftable[0].append('Pre')
 cftable[1].append('Dif')
@@ -34,15 +34,17 @@ import gzip
 with gzip.open(filename, 'rt') as f:	
 	for i, line in enumerate(f, 1): #contents is file as a string
 		if i % 4 == 2:
-			seq=line #store the seq
+			seq=line.rstrip() #store the seq, gets rid of white space/line afterwards, \n is last char
 			seqno=round((i/4)+0.1) #because 0.5 gets rounded down
-			errorno={seqno:0} 
+			errorno={seqno:0}
+			diffno={seqno:0} 
 			nextline= True #nextline, how specify, add 1 to i?
 			if nextline: #inside the loop
 				diffsline = gzip.open(filename, 'rt').readlines()[i] #reads file starting at 0
 				diffs=diffsline.split() #makes a list from changes line, separated by spaces 
 			nextline= False
 			for d in diffs: #and s in seqs to get them to align?
+				diffno[seqno]+=1
 				pos=re.search(r'\d+',d) #search should get me first number found in each element, + sign means you have to find at least one
 				if pos: #if you get something, to allow for no diffs
 					#print (pos) #is a whole line with actual no. at end
@@ -52,43 +54,43 @@ with gzip.open(filename, 'rt') as f:
 						if l != cons:                         
 							if l in d: #look for l in a single diff
 								counter[l+cons]+=1 
-								errorno[seqno]+=1 #counts diffs for each diffsline #DIFFFFFFFFF
-								ftable[1].append(str(errorno[seqno]))
+								errorno[seqno]+=1 #counts diffs for each diffsline 
+								ftable[2].append(str(errorno[seqno]))
+								ftable[1].append(str(diffno[seqno]))
 								ftable[0].append(str(seqno))
-								ftable[3].append(l) #all lists of string
-								ftable[4].append(cons)
+								ftable[4].append(l) #all lists of string
+								ftable[5].append(cons)
 								#go back and get chars, shorter way? slices
 								if posno==0: # == checks if two things have the same value
-									prev=' '
+									prev='  '
 									post=seq[posno+1:(posno+1+flankno)] #slicing is inclusive of bounds, if flankno 2, like saying 1:2
 								elif posno+1<=flankno: 
 									prev=' '+seq[:posno]
 									post=seq[posno+1:(posno+1+flankno)]
 								elif len(seq)-1==posno: #-1 to manage offset, make same as posno, start at 0
-									post=' ' #going to a newline???
-									prev=seq[posno-2:posno]	
+									post='  ' #2 spaces 
+									prev=seq[posno-flankno:posno]	
 								elif posno+1>(len(seq)-flankno): #+1 to make it same as flankno, starting at 1
 									post=seq[posno+1:]+' '
-									prev=seq[posno-2:posno]
+									prev=seq[posno-flankno:posno]
 								else:
-									prev=seq[posno-2:posno]
+									prev=seq[posno-flankno:posno]
 									post=seq[posno+1:(posno+1+flankno)]
-								ftable[2].append(prev)
-								ftable[5].append(post)
-								#sys.stdout=open('flank table.txt', 'w')
-								#for row in zip(ftable[0],ftable[1],ftable[2],ftable[3],ftable[4],ftable[5]): #wants a str input #3rd element requires iteration? as many rows to match up with the first two arguments
-									#print('\t'.join(row)) #doesn't cumulative print
+								ftable[3].append(prev)
+								ftable[6].append(post)
+								with open('flank table.txt', 'w') as ft:
+									for row in zip(ftable[0],ftable[1],ftable[2],ftable[3],ftable[4],ftable[5],ftable[6]): #wants a str input #3rd element requires iteration? as many rows to match up with the first two arguments
+										ft.write('\t'.join(row)) #doesn't cumulative print
+										ft.write('\n')
 								#increment counter for that error seq
-								fcounter[prev+l+cons+post]+=1
-								fcounter[prev+l+cons+'..']+=1
-								fcounter['..'+l+cons+post]+=1
-								fcounter[prev+'.'+'.'+post]+=1
-								fcounter[prev+'.'+'.'+'..']+=1
-								fcounter['..'+'.'+'.'+post]+=1
+								cfcounter[prev+l+cons+post]+=1
+								cfcounter[prev+l+cons+'  ']+=1
+								cfcounter['  '+l+cons+post]+=1
+								cfcounter[prev+' '+' '+post]+=1
+								cfcounter[prev+' '+' '+'  ']+=1
+								cfcounter['  '+' '+' '+post]+=1
 
-								print (fcounter)
-
-	sys.stdout=open('Count of error types table.txt', 'w')#w means write, output will print to file instead of console, global 
+with open('Count of error types table.txt', 'w') as ct: #w means write, output will print to file instead of console, global 
 	for c in counter: #each counter is a line in the table
 		#list of lists method
 		splitc = re.findall('.', c) #splits c after a single position
@@ -98,20 +100,23 @@ with gzip.open(filename, 'rt') as f:
 	ctable[2]=[str(counter[c]) for counter[c] in ctable[2]] #or do this list comp
 	#zip and joinrow method
 	for row in zip(ctable[0],ctable[1],ctable[2]): #wants a str input #3rd element requires iteration? as many rows to match up with the first two arguments
-		print('\t'.join(row)) #tab separated 
+		ct.write('\t'.join(row)) #tab separated 
+		ct.write('\n')
 
-	print('\n')
-	#count flanks table, ONLY GOT 1 RESULT 
-	for k in fcounter: #allow for differing key size
-		splitk = re.findall('.',k) #len is 6 elements, even when you get full stops
+	ct.write('\n')
+	#count flanks table
+	scfcounter=OrderedDict(reversed(sorted(cfcounter.items(), key=lambda t: t[1]))) #don't get, descending value
+	for k in scfcounter: 
+		splitk = re.findall('.',k) #len is 6 elements
 		cftable[0].append(splitk[0]+splitk[1]) 
 		cftable[1].append(splitk[2])
 		cftable[2].append(splitk[3])
-		cftable[3].append(splitk[4]+splitk[5]) #out of range error??
-		cftable[4].append(str(fcounter[k]))
+		cftable[3].append(splitk[4]+splitk[5])
+		cftable[4].append(str(scfcounter[k]))
 	for row in zip(cftable[0],cftable[1],cftable[2],cftable[3],cftable[4]):
-		print('\t'.join(row)) 
+		ct.write('\t'.join(row)) 
+		ct.write('\n')
 
 #NEXT
-#fix count flanks, not working
+#change error and diff counters
 #run on whole file
